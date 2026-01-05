554a5102d52e4f4560be8f6f0725643564d64dc1
Ci$c0 <emmanueloduro37@gmail.com>
2026-01-05 15:19:33 +0000

    Configured API models

---
+++ api/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+from django.utils import timezone
+
+User = settings.AUTH_USER_MODEL
+
+# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    duration = models.PositiveIntegerField(help_text = "Duration")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
+    category = models.ForeignKey(Categories, related_name='questions', on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.text
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    is_correct = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.is_correct:
+            Choice.objects.filter(
+                question=self.question,
+                is_correct=True
+            ).update(is_correct=False)
+        super().save(*args, **kwargs)
+    
+class Attempt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    started_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    def is_expired(self):
+        elapsed = (timezone.now() - self.started_at).total_seconds()
+        
+        return elapsed > self.quiz.duration
