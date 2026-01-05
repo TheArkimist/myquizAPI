fdf64c8a9f0d1a35cc2f9f0f92a516a5a6ffb86f
Ci$c0 <emmanueloduro37@gmail.com>
2026-01-05 15:39:52 +0000

    Configured API urls and views for endpoints

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import CategoriesListView, QuizListView, SubmitQuizView, QuizByCategaoryView, RandomQuestionsByCategoryView, StartQuizView
+
+
+urlpatterns = [
+    path('categories/', CategoriesListView.as_view()),
+    path('categories/<slug:slug>/quizzes/', QuizByCategaoryView.as_view()),
+    path('categories/<slug:slug>/questions/', RandomQuestionsByCategoryView.as_view()),
+    path('quiz/', QuizListView.as_view()),
+    path('quiz/<int:quiz_id>/submit/', StartQuizView.as_view()),
+    path('quiz/<int:quiz_id>/start/', SubmitQuizView.as_view())
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Quiz, Choice, Attempt, Categories, Question
+from .serializers import QuizSerializer, CategoriesSerializer, QuestionSerializer
+import random
 
 # Create your views here.
+class CategoriesListView(APIView):
+    
+
+    def get(self, request):
+        categories = Categories.objects.all()
+        serializer = CategoriesSerializer(categories, many=True)
+
+        return Response(serializer.data)
+    
+class QuizListView(APIView):
+
+    def get(self, request):
+        quizzes = Quiz.objects.all()
+        serializer = QuizSerializer(quizzes, many=True)
+
+        return Response(serializer.data)
+    
+class QuizByCategaoryView(APIView):
+    def get(self, request, slug):
+        quiz = Quiz.objects.filter(
+            questions__category__slug = slug
+        ).distinct()
+
+        serializer = QuizSerializer(quiz, many=True)
+
+        return Response(serializer.data)
+    
+class RandomQuestionsByCategoryView(APIView):
+    def get(self, request, slug):
+        limit = int(request.query_params.get('limit', 10))
+        questions = list(Question.objects.filter(category__slug=slug))
+        random.shuffle(questions)
+        serializer = QuestionSerializer(
+            questions[:limit],
+            many = True
+        ) 
+
+        return Response(serializer.data)
+    
+class StartQuizView(APIView):
+
+    def post(self, request, quiz_id):
+        quiz = Quiz.objects.get(id=quiz_id)
+        attempt = Attempt.objects.create(
+            user = request.user,
+            quiz = quiz
+        )
+
+        return Response({
+            "attempt_id": attempt.id,
+            "quiz_id": quiz_id,
+            "duration": quiz.duration,
+            "started_at": attempt.started_at
+        })
+    
+class SubmitQuizView(APIView):
+
+    def post(self, request, quiz_id):
+        attempt = Attempt.objects.filter(
+            user = request.user,
+            quiz_id = quiz_id,
+            submitted_at__isnull=True
+        ).latest('started_at')
+
+        if attempt.is_expired():
+            return Response(
+                {"detail": "Time expired"}, status=status.HTTP_403_FORBIDDEN
+            )
+
+        answers = request.data.get('answers', {})
+        score = 0
+
+        for question_id, choice_id in answers.items():
+            if Choice.objects.filter(
+                id=choice_id,
+                question_id=question_id,
+                is_correct=True
+            ).exists():
+                score += 1
+
+        return Response({
+            "score": score,
+            "attempt_id": attempt.id
+        }, status=status.HTTP_201_CREATED)
+    
